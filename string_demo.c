44db320fb4df338c788a245d2d169428c1e4d1a4
hongwilliam <willhong20@gmail.com>
2017-09-28 21:15:34 -0400

    did demo for strcpy and strncpy

---
+++ string_demo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+  /* demo 1: strcpy
+  copies the string pointed to by s2 into the object pointed to by s1
+  strcpy(char *s1, char *s2)
+  s1 - array where s2 will be copied to, s2 - string to be copied */ 
+  
+
+  char s1[256]; 
+  strcpy(s1, "tom brady"); //would return s1
+  printf("%s\n", s1); //should return "tom brady"
+
+
+  /* demo 2: strncpy
+  copies up to n characters from the string pointed to by string2 to string1
+  strncpy(char *s1, char *s2, n)
+  s1 - array where s2 will be copied to, s2 - string to be copied
+  n - number of characters to copy */
+
+  char s2[256];
+  strncpy(s2, "peyton manning", 4);
+  printf("%s\n", s2); //should return "peyt"
+
+}
+  
+  
