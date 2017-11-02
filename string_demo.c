912835675780a00480e3c580d261c8ebf4a393fb
hongwilliam <willhong20@gmail.com>
2017-11-02 13:54:09 -0400

    Added comments to indicate which functions were tested

--- string_demo.c
+++ string_demo.c
@@ -7,10 +7,12 @@ int main(){
   /* demo 1: strcpy
   copies the string pointed to by s2 into the object pointed to by s1
   strcpy(char *s1, char *s2)
-  s1 - array where s2 will be copied to, s2 - string to be copied */ 
-  
+  s1 - array where s2 will be copied to, s2 - string to be copied */
 
-  char s1[256]; 
+
+  char s1[256];
+  printf("\nTesting out strcpy...\n");
+  printf("copying 'tom brady' into an empty string and returning that string\n");
   strcpy(s1, "tom brady"); //would return s1
   printf("%s\n", s1); //should return "tom brady"
 
@@ -22,9 +24,9 @@ int main(){
   n - number of characters to copy */
 
   char s2[256];
+  printf("\nTesting out strncpy... \n");
+  printf("copying the first 4 chars of 'peyton manning' into an empty string and returning that string\n");
   strncpy(s2, "peyton manning", 4);
   printf("%s\n", s2); //should return "peyt"
 
 }
-  
-  
